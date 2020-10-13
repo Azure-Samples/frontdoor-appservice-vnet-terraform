28c402db69edb72673a85b396e1f58a53af61cfc
Jyotsna Ravikumar <jyraviku@microsoft.com>
2020-10-13 16:06:03 -0700

    adding app and appsvc plan

--- main.tf
+++ main.tf
@@ -11,6 +11,37 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
 }
 
+resource "azurerm_app_service_plan" "asp" {
+  name                = "rocket-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+
+resource "azurerm_app_service" "apsvc" {
+  name                = "rocket-appservice"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    linux_fx_version = "COMPOSE|${filebase64("docker-compose.yaml")}"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://mcr.microsoft.com",
+    "DOCKER_REGISTRY_SERVER_USERNAME" = "",
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = "",
+  }
+}
+
 resource "azurerm_frontdoor_firewall_policy" "demowafpolicy" {
   name                              = "demowafpolicy"
   resource_group_name               = azurerm_resource_group.rg.name
